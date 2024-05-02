8dd04d1a070c1f3b2691debaab7c0764b3eb02f3
Benhadid-omaima <oumaima.benhadid@esprit.tn>
2024-05-02 01:22:06 +0100

    stat+mail

--- EspritE-LearnWeb-main/src/Controller/PresenceController.php
+++ EspritE-LearnWeb-main/src/Controller/PresenceController.php
@@ -4,13 +4,17 @@ namespace App\Controller;
 
 use App\Entity\Presence;
 use App\Form\PresenceType;
+use App\Repository\ClasseRepository;
 use App\Repository\PresenceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 #[Route('/presence')]
 class PresenceController extends AbstractController
 {
@@ -21,6 +25,74 @@ class PresenceController extends AbstractController
             'presences' => $presenceRepository->findAll(),
         ]);
     }
+    
+
+   
+
+
+    #[Route('/mail', name: 'app_mail', methods: ['GET'])]
+    public function sendEmail(MailerInterface $mailer,PresenceRepository $presenceRepository, ClasseRepository $userRepository): Response
+{
+    $idetudtot = [];
+    foreach( $presenceRepository->findAll() as $pres  ){
+        foreach(explode("," , $pres->getIdpresence()) as $ide){
+            
+            $idetud[] = intval($ide);
+        }
+        array_pop($idetud);
+        foreach($idetud as $iid){
+            $name[]  = $userRepository->find($iid)->getNomClasse
+            ();
+        }
+        $idetudtot[]=$name;
+        $idetud =[]; 
+        $name=[] ; 
+    }
+
+    $pdfOptions = new Options();
+    $pdfOptions->set('defaultFont', 'Arial');
+    $dompdf = new Dompdf($pdfOptions);
+        //$abonnement=$AbonnementRepository->findAll();
+
+        //$html2pdf = new Html2Pdf();
+
+        // Retrieve the HTML generated in our twig file
+        $html = $this->renderView('presence/pdf.html.twig',
+            [
+                "presences"=> $presenceRepository->findAll(),
+                "nometu"=>$idetudtot,
+                ]);
+
+        // Load HTML to Dompdf
+        $dompdf->loadHtml($html);
+        //$html2pdf->writeHTML($html);
+
+        //$html2pdf->output('myPdf.pdf');
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+
+
+        // Render the HTML as PDF
+        $dompdf->render();
+        $output = $dompdf->output();
+        $pdfFilepath ="test.pdf";
+        file_put_contents($pdfFilepath, $output);
+        
+        $email = (new Email())
+        ->from('omaimabhd3@gmail.com')
+        ->to('baymarou@gmail.com')
+        ->subject('liste des presences')
+        ->text('This is a test email sent from Symfony Mailer.')
+        ->attach($output,"Presences.pdf");
+
+    $mailer->send($email);
+    flash()->addSuccess('mail envoyé avec succés');
+
+    return $this->redirectToRoute('app_presence_index', [], Response::HTTP_SEE_OTHER);
+}
+
+    
 
     #[Route('/new', name: 'app_presence_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
